569cb53888b618031801f414631b826a7b1292d2
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-30 14:55:03 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Paint Job!");
+
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+
+        System.out.println(getBucketCount(0.75,0.75,0.5,0));
+
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
+    }
+
+    public static int getBucketCount(double width, double height,double areaPerBucket, int extraBuckets){
+        if(width <= 0 || height <= 0 ||areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+        double area = width * height;
+        int totalBucketsNeeded = (int) Math.ceil(area / areaPerBucket);
+        int additionalBucketsNeeded = totalBucketsNeeded - extraBuckets;
+        return additionalBucketsNeeded > 0 ? additionalBucketsNeeded : 0;
+
+    }
+
+    public static int getBucketCount(double width, double height,double areaPerBucket){
+        if(width <= 0 || height <= 0 ||areaPerBucket <= 0){
+            return -1;
+        }
+        double area = width * height;
+        int totalBucketsNeeded = (int) Math.ceil(area / areaPerBucket);
+        return totalBucketsNeeded;
+
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(area <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        int totalBucketsNeeded = (int) Math.ceil(area / areaPerBucket);
+        return totalBucketsNeeded;
     }
 }
